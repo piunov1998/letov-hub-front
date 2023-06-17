ad3f51d73fa069636813df752af2a431a53a219e
Anton Piunov <anton.piunov@cyberprotect.ru>
2023-06-17 23:36:34 +0300

    [ref]: перевел SongList на классы

--- src/components/SongList.tsx
+++ src/components/SongList.tsx
@@ -1,32 +1,38 @@
 import Song, {songProps} from "./Song";
-import React, {useState} from "react";
+import React from "react";
 import axios from "axios";
 import "../css/SongList.css"
 
-function getSongs(setter: React.Dispatch<React.SetStateAction<songProps[]>>): songProps[] {
-    let songs: songProps[] = []
-    const url = `${process.env.REACT_APP_HOST}/api/songs`
-    axios.get<songProps[]>(url, {withCredentials: true}).then((response) => {
-        setter(response.data)
-    })
-    return songs
+type songListState = {
+    songs: songProps[]
 }
 
-function SongList() {
-
-    const [songList, setSongList] = useState<songProps[]>([])
-    const [loaded, setLoaded] = useState<boolean>(false)
-
-    if (!loaded) {
-        getSongs(setSongList)
-        setLoaded(true)
+class SongList extends React.Component<any, songListState> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            songs: []
+        }
+        this.getSongs()
     }
 
-    return (
-        <div className="song-list">{songList.map((song: songProps) => (
-            <Song id={song.id} name={song.name} source={song.source} key={song.id}/>
-        ))}</div>
-    )
+    getSongs(): songProps[] {
+        let songs: songProps[] = []
+        const url = `${process.env.REACT_APP_HOST}/api/songs`
+        axios.get<songProps[]>(url, {withCredentials: true}).then((response) => {
+            this.setState({songs: response.data})
+        })
+        return songs
+    }
+
+    render() {
+        return (
+            <div className="song-list">{this.state.songs.map((song: songProps) => (
+                <Song id={song.id} name={song.name} source={song.source} key={song.id}/>
+            ))}</div>
+        )
+    }
 }
+
 
 export default SongList
