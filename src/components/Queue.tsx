9f6218f721aacd0cdf6462bef5c42b447786d144
Anton Piunov <anton.piunov@cyberprotect.ru>
2023-06-17 19:59:01 +0300

    [ref]: перевел Queue на классы

--- src/components/Queue.tsx
+++ src/components/Queue.tsx
@@ -1,34 +1,40 @@
 import QueueItem, {queueItemProps} from "./QueueItem";
-import React, {useState} from "react";
+import React from "react";
 import axios from "axios";
 import "../css/Queue.css"
 
-function getQueue(setter: React.Dispatch<React.SetStateAction<queueItemProps[]>>) {
-    const url = `${process.env.REACT_APP_HOST}/api/queue`
-        axios.get<queueItemProps[]>(url, {withCredentials: true}).then((response) => {
-            setter(response.data)
-    })
+
+type queueState = {
+    queue: queueItemProps[]
 }
 
-function Queue() {
-
-    const [queue, setQueue] = useState<queueItemProps[]>([])
-    const [loaded, setLoaded] = useState<boolean>(false)
-
-    if (!loaded) {
-        getQueue(setQueue)
-        setLoaded(true)
+class Queue extends React.Component<any, queueState> {
+    constructor(props: any) {
+        super(props)
+        this.state = {
+            queue: []
+        }
+        this.getQueue()
     }
 
-    return (<div>
-        {
-            queue.length > 0 ? <div className="queue">
-                {queue.map((item: queueItemProps) => (
-                    <QueueItem id={item.id} song={item.song} user={item.user} key={item.id}/>
-                ))}
-            </div> : <div className="queue-placeholder">No items</div>
-        }
-    </div>)
+    getQueue() {
+        const url = `${process.env.REACT_APP_HOST}/api/queue`
+        axios.get<queueItemProps[]>(url, {withCredentials: true}).then((response) => {
+            this.setState({queue: response.data})
+        })
+    }
+
+    render() {
+        return (<div>
+            {
+                this.state.queue.length > 0 ? <div className="queue">
+                    {this.state.queue.map((item: queueItemProps) => (
+                        <QueueItem id={item.id} song={item.song} user={item.user} key={item.id}/>
+                    ))}
+                </div> : <div className="queue-placeholder">No items</div>
+            }
+        </div>)
+    }
 }
 
 export default Queue
